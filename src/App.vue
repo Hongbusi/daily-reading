ea479a2426020313930108c67d55195aca28cd21
hongyongchun <hongyongchun@edusoho.com>
2020-09-25 11:44:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app">
+    Welcome to Hongbusi website.
+
+    <router-view/>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    this is home page.
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
